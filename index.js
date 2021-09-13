e44b218dc0cf2bae6fc96deb3b086b707502cb9b
Eduardolovo <eduardo.llovo@gmail.com>
2021-09-13 20:45:52 -0300

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//     require('dotenv').config();
+// }
 const express = require('express');
 
 const app = express();
@@ -8,20 +8,20 @@ app.use(express.json());
 
 const Conn = require('./models/conn/conn');
 
-const db_url = process.env.DB_URL;
-const db_user = process.env.DB_USER;
-const db_pass = process.env.DB_PASS;
-const db_data = process.env.DB_DATA;
+// const db_url = process.env.DB_URL;
+// const db_user = process.env.DB_USER;
+// const db_pass = process.env.DB_PASS;
+// const db_data = process.env.DB_DATA;
 
-Conn(db_url, db_user, db_pass, db_data);
+// Conn(db_url, db_user, db_pass, db_data);
 
-// Conn("localhost", 27017, "tarefas");
+Conn("localhost", 27017, "tarefas");
 
 const port = 3000;
 
 const tarefasRouter = require('./routers/tarefas.routes');
 app.use('/tarefas', tarefasRouter);
 
-app.listen(process.env.PORT || port, () => {
+app.listen(port, () => {
     console.info(`Servidor rodando na porta ${port}`);
 })
