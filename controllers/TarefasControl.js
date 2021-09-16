0b1743a50a20af61cd0430e19d9a69cd3fc04cd8
Eduardolovo <eduardo.llovo@gmail.com>
2021-09-16 00:46:45 -0300

    final controller

--- controllers/TarefasControl.js
+++ controllers/TarefasControl.js
@@ -3,7 +3,7 @@ const tarefaModel = require("../models/tarefas");
 const getTarefa = async (req, res) => {
   await tarefaModel.find({}, (err, tarefa) => {
     if (err) {
-      return res.status(400).send(`Tarefa invalida ${err}`);
+      return res.status(400).send("Tarefa invalida");
     }
     if (!tarefa) {
       return res.status(404).send("Tarefa não encontrada");
@@ -11,6 +11,8 @@ const getTarefa = async (req, res) => {
     res.status(200).send(tarefa);
   });
 };
+
+
 const getTarefaById = async (req, res) => {
   await tarefaModel.find({ _id: req.params.id }, (err, tarefa) => {
     if (err) {
@@ -19,35 +21,42 @@ const getTarefaById = async (req, res) => {
     res.status(200).send(tarefa);
   });
 };
+
+
 const createTarefa = (req, res) => {
   const body = req.body;
   if (!body) {
-    return res.status(400).send(`Tarefa invalida`);
+    return res.status(400).send("Tarefa invalida");
   }
   const tarefa = new tarefaModel(body);
   tarefa
     .save()
-    .then(() => res.status(201).send(`Tarefa ${body.title}  criada com sucesso`))
+    .then(() => res.status(201).send("Tarefa criada com sucesso"))
     .catch(() =>
       res.status(400).send(`Não foi possivel criar tarefa, tente novamente`)
     );
 };
+
+
 const updateTarefa = async (req, res) => {
   const body = req.body;
   if (!body) {
-    return res.status(400).send(`Tarefa invalida, tente novamente`);
+    return res.status(400).send("Tarefa invalida, tente novamente");
   } else {
     await tarefaModel
       .updateOne({ _id: req.params.id }, req.body)
       .then(() => res.status(200).send("Tarefa atualizada com sucesso"))
-      .catch(() => res.status(400).send(`Não foi possivel atualizar tarefa, tente novamente`));
+      .catch(() => res.status(400).send("Não foi possivel atualizar tarefa, tente novamente"));
   }
 };
+
+
 const deleteTarefa = async (req, res) => {
   await tarefaModel
     .deleteOne({_id: req.params.id })
     .then(() => res.status(200).send("Tarefa deletada com sucesso"))
-    .catch((err) => res.status(404).send(`Tarefa não encontrada: ${err.message}`));
+    .catch((err) => res.status(404).send("Tarefa não encontrada"));
 };
+
 
 module.exports = { getTarefa, getTarefaById, createTarefa, updateTarefa, deleteTarefa };
--- routers/tarefas.routes.js
+++ routers/tarefas.routes.js
@@ -3,51 +3,15 @@ const router = express.Router();
 const Tarefa = require('../models/tarefas');
 const tarefaController = require('../controllers/TarefasControl')
 
-router.get('/', async (req, res) => {
-    await Tarefa.find({})
-    .then((tarefa) => {
-        res.send(tarefa);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
-
+router.get('/', tarefaController.getTarefa);
 
 router.post('/add', tarefaController.createTarefa);
 
-router.get('/findById/:id', async (req, res) => {
-    await Tarefa.find({_id : req.params.id})
-    .then((tarefa) => {
-        res.status(200).send(tarefa);
-    })
-    .catch((err) => {
-        res.status(400).send("Erro ao pesquisar tarefa, tente novamente");
-        console.error(err);
-    })
-});
+router.get('/findById/:id', tarefaController.getTarefaById);
 
-router.put('/update/:id', async (req, res) => {
-    await Tarefa.updateOne({_id : req.params.id}, req.body)
-    .then(() => {
-        res.status(200).send("Tarefa atualizada");
-    })
-    .catch((err) => {
-        res.status(400).send("Erro ao atualizar tarefa, tente novamente");
-        console.log(err);
-    });
-});
+router.put('/update/:id', tarefaController.updateTarefa);
 
-router.delete('/delete/:id', async (req, res) => {
-    await Tarefa.deleteOne({_id : req.params.id})
-    .then(() => {
-        res.status(200).send("Tarefa deletada com sucesso");
-    })
-    .catch((err) => {
-        res.status(400).send("Erro ao deletar tarefa, tente novamente");
-        console.er(err);
-    });
-});
+router.delete('/delete/:id', tarefaController.deleteTarefa);
 
 module.exports = router;
 
